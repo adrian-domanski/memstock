e633697a6b4b7fc293ac4a38b116713d94ed1db7
unknown <adrian.domanski.dev@gmail.com>
2020-07-29 20:02:02 +0200

    strapi init

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1927111215d8d32a7e861282ec163b77'),
+    },
+  },
+});
