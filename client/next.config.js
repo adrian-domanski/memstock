6d6da0c85f01ce4bc46a997e0f3ccfa1db30d818
unknown <adrian.domanski.dev@gmail.com>
2020-08-26 10:07:07 +0200

    added next.config.js

---
+++ client/next.config.js
@@ -0,0 +1,14 @@
+const withPWA = require("next-pwa");
+const isDev = process.env.NODE_ENV === "development";
+require("dotenv").config();
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: isDev,
+  },
+  env: {
+    SERVER_URL: isDev ? "http://localhost:1337" : "https://api.memstock.pl",
+    CLIENT_URL: isDev ? "http://localhost:3000" : "https://memstock.pl",
+  },
+});
