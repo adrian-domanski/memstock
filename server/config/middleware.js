267ac874664c0bd61e22de20b59ffd7014df489b
Adrian <adrian.domanski.dev@gmail.com>
2020-12-14 20:00:01 +0100

    Server config, added api to cors

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     cors: {
-      // origin: ['https://memstock.pl', 'https://www.memstock.pl']
+      // origin: ['https://memstock.pl', 'https://www.memstock.pl', 'https://api.memstock.pl', 'https://www.api.memstock.pl']
     },
   },
 };
