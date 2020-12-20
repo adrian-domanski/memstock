25846c1594e9382b3e6f8b452e98ac74840cc3b4
Adrian <adrian.domanski.dev@gmail.com>
2020-12-20 12:10:31 +0100

    Added server URL to .env

--- server/config/server.js
+++ server/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("SERVER_URL"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1927111215d8d32a7e861282ec163b77'),
+      secret: env("ADMIN_JWT_SECRET", "1927111215d8d32a7e861282ec163b77"),
     },
   },
 });
