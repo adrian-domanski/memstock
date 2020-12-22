d056b4aa15ce7e134380161874295cbd6f2d77c6
Adrian <adrian.domanski.dev@gmail.com>
2020-12-22 15:57:13 +0100

    Login provider error handler

--- client/pages/connect/[provider].tsx
+++ client/pages/connect/[provider].tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { SingletonRouter, withRouter } from "next/router";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout/Layout";
 import { AuthContext } from "../../context/authContext";
 import {
@@ -16,32 +16,40 @@ interface IProps {
 
 const Redirect: React.FC<IProps> = ({ router }) => {
   const { dispatch } = useContext(AuthContext);
+  const [error, setError] = useState(false);
   const params = router.query;
 
   useEffect(() => {
     if (params.access_token) {
-      fetch(
-        `${process.env.SERVER_URL}/auth/${params.provider}/callback?access_token=${params.access_token}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch({
-            type: "LOGIN_SUCCESS",
-            payload: {
-              user: data.user,
-              token: data.jwt,
-            },
+      try {
+        fetch(
+          `${process.env.SERVER_URL}/auth/${params.provider}/callback?access_token=${params.access_token}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            dispatch({
+              type: "LOGIN_SUCCESS",
+              payload: {
+                user: data.user,
+                token: data.jwt,
+              },
+            });
           });
-        });
+        router.push("/");
+      } catch (err) {
+        console.log(err);
+        console.log({ ...err });
+        // Account with such email was created using standard registration method
+        setError(true);
+      }
     }
-    router.push("/");
   }, []);
 
   return (
     <Layout popularMems topUsers>
       <ContentBody className="py-6">
         <PageWrapper>
-          {params.access_token ? (
+          {!error ? (
             <>
               <StyledTitle className="has-text-centered my-3 px-4">
                 Zalogowano pomyślnie, za chwilę nastąpi przekierowanie...
@@ -53,7 +61,9 @@ const Redirect: React.FC<IProps> = ({ router }) => {
           ) : (
             <>
               <StyledTitle className="has-text-centered my-3 px-4">
-                Nie udało się zalogować, za chwilę nastąpi przekierowanie...
+                Nie udało się zalogować. Konto o takim adresie email istnieje
+                już w naszym serwisie i jest stworzone za pomocą standardowej
+                metody rejestracji.
               </StyledTitle>
               <Link href="/">
                 <Button className="is-primary light mt-5">Strona główna</Button>
