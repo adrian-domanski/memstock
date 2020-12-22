a386de886dba37226e26951d51fcbb2e964f9f2c
Adrian <adrian.domanski.dev@gmail.com>
2020-12-22 17:02:26 +0100

    Login provider errors messages

--- client/pages/connect/[provider].tsx
+++ client/pages/connect/[provider].tsx
@@ -14,9 +14,16 @@ interface IProps {
   router: SingletonRouter;
 }
 
+enum ErrorTypes {
+  EMAIL_TAKEN,
+  INVALID_TOKEN,
+  OTHER,
+  NONE,
+}
+
 const Redirect: React.FC<IProps> = ({ router }) => {
   const { dispatch } = useContext(AuthContext);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState<ErrorTypes>(ErrorTypes.NONE);
   const params = router.query;
 
   useEffect(() => {
@@ -26,7 +33,6 @@ const Redirect: React.FC<IProps> = ({ router }) => {
       )
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           if (data?.user && data?.jwt) {
             dispatch({
               type: "LOGIN_SUCCESS",
@@ -35,12 +41,22 @@ const Redirect: React.FC<IProps> = ({ router }) => {
                 token: data.jwt,
               },
             });
+            router.push("/");
           } else {
-            // Account with such email is already created using standard - register method (email taken)
-            setError(true);
+            if (
+              data?.message[0]?.messages[0]?.id ===
+              "Auth.form.error.email.taken"
+            ) {
+              setError(ErrorTypes.EMAIL_TAKEN);
+            } else if (
+              data?.message?.error?.message === "Invalid OAuth access token."
+            ) {
+              setError(ErrorTypes.INVALID_TOKEN);
+            } else {
+              setError(ErrorTypes.OTHER);
+            }
           }
         });
-      router.push("/");
     }
   }, []);
 
@@ -48,7 +64,7 @@ const Redirect: React.FC<IProps> = ({ router }) => {
     <Layout popularMems topUsers>
       <ContentBody className="py-6">
         <PageWrapper>
-          {!error ? (
+          {error === ErrorTypes.NONE ? (
             <>
               <StyledTitle className="has-text-centered my-3 px-4">
                 Zalogowano pomyślnie, za chwilę nastąpi przekierowanie...
@@ -57,12 +73,34 @@ const Redirect: React.FC<IProps> = ({ router }) => {
                 <Button className="is-primary light mt-5">Strona główna</Button>
               </Link>
             </>
+          ) : error === ErrorTypes.EMAIL_TAKEN ? (
+            <>
+              <StyledTitle className="has-text-centered has-text-danger my-3 px-4">
+                Nie udało się połączyć kont. Konto o takim adresie email
+                istnieje już w naszym serwisie i jest stworzone za pomocą
+                standardowej metody rejestracji. Zaloguj się za pomocą
+                poniższego odnośnika.
+              </StyledTitle>
+              <Link href="/logowanie">
+                <Button className="is-primary mt-5">Zaloguj się</Button>
+              </Link>
+              <Link href="/">
+                <Button className="is-primary light mt-5">Strona główna</Button>
+              </Link>
+            </>
+          ) : error === ErrorTypes.INVALID_TOKEN ? (
+            <>
+              <StyledTitle className="has-text-centered has-text-danger my-3 px-4">
+                Wystąpił błąd: Niepoprawny token uwierzytelniający.
+              </StyledTitle>
+              <Link href="/">
+                <Button className="is-primary light mt-5">Strona główna</Button>
+              </Link>
+            </>
           ) : (
             <>
-              <StyledTitle className="has-text-centered my-3 px-4">
-                Nie udało się zalogować. Konto o takim adresie email istnieje
-                już w naszym serwisie i jest stworzone za pomocą standardowej
-                metody rejestracji.
+              <StyledTitle className="has-text-centered has-text-danger my-3 px-4">
+                Wystąpił nieoczekiwany błąd, przepraszamy na utrudniena.
               </StyledTitle>
               <Link href="/">
                 <Button className="is-primary light mt-5">Strona główna</Button>
