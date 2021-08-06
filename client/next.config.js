23ccb879c6736b2357da8cd6a1daae694f5b6e02
Adrian <adrian.domanski.dev@gmail.com>
2021-08-06 10:57:11 +0200

    Change api and client URL

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,16 @@
-const withPWA = require("next-pwa");
-const isDev = process.env.NODE_ENV === "development";
+const withPWA = require('next-pwa');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = withPWA({
   pwa: {
-    dest: "public",
+    dest: 'public',
     disable: isDev,
   },
   env: {
     isDev,
-    SERVER_URL: isDev ? "http://localhost:5010" : "https://api.memstock.pl",
-    CLIENT_URL: isDev ? "http://localhost:3000" : "https://memstock.pl",
+    SERVER_URL: isDev
+      ? 'http://localhost:5010'
+      : 'https://api.memstock.kodario.pl',
+    CLIENT_URL: isDev ? 'http://localhost:3000' : 'https://memstock.kodario.pl',
   },
 });
